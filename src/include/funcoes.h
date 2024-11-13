9528e1c4c4bb9ec1139d4ba599a4d27d0c68c9b6
PedroPinheiro2005 <pedrodasilvapinheiro23@gmail.com>
2024-11-12 21:50:21 -0300

    Updated funcoes_h
    Added the functions to the funcoes_h file, as well as correcting some syntax.

--- src/include/funcoes.h
+++ src/include/funcoes.h
@@ -1,13 +1,25 @@
 #ifndef FUNCOES_H
 #define FUNCOES_H
 
-//Declaracao das funcoes
+void tela_clie();
+void limpa_tela();
+void ordena_codigo();
+void ordena_nome();
+void leitura();
+void mostra_cliente();
+void cadastrar();
+void consultar_cliente();
+void consultar();
+void cad_inicio();
+void cad_posicao();
+void remove_final();
+void remove_posicao();
+void remove_inicio();
+void consulta_todos();
+void altera_cliente();
+void menu_consultar();
 
-void cadastro_conta();
-
-void gotoxy(int x, int y);
-
-
+#endif // FUNCOES_H
 
 
 
